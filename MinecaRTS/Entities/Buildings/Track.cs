4204d0b5da508a3388d98a370e0ded51161d28cd
Matt Noone <matt.noone95@gmail.com>
2017-08-14 11:41:32 +1000

    QUALITY: Sync local files for desktop cleanup

--- MinecaRTS/Entities/Buildings/Track.cs
+++ MinecaRTS/Entities/Buildings/Track.cs
@@ -8,13 +8,32 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace MinecaRTS
 {
+    /// <summary>
+    /// The Minecart Tracks in the game. Minecarts move much faster on these.
+    /// Unlike other buildings, Tracks can be walked over.
+    /// </summary>
     public class Track : Building
     {
+        /// <summary>
+        /// Max health for tracks.
+        /// </summary>
         public const int MAX_HEALTH = 50;
 
+        /// <summary>
+        /// Texture to be rendered while active.
+        /// </summary>
         public static Texture2D ACTIVE_TEXTURE;
+
+        /// <summary>
+        /// Texture to be rendered while being constructed.
+        /// </summary>
         public static Texture2D CONSTRUCTION_TEXTURE;
 
+        /// <summary>
+        /// Initializes a new Track.
+        /// </summary>
+        /// <param name="pos">The position</param>
+        /// <param name="team">The team the track belongs to</param>
         public Track(Vector2 pos, Team team) : base (pos, new Vector2(31, 31), team, MAX_HEALTH, ACTIVE_TEXTURE, CONSTRUCTION_TEXTURE)
         {
         }
